c7742f899a1b51241d685c3c78b161ba83c6f92f
Darshit Desai <darshitdesai43@gmail.com>
2018-02-04 16:12:08 -0400

    Lab 2 Upload

---
+++ COMP305Lab2/Assets/Scripts/Button2ClickHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button2ClickHandler : MonoBehaviour {
+
+    public GameObject gameObject2;
+
+    public Sprite newImage1;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rBody = gameObject2.GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        SpriteRenderer sRend = gameObject2.GetComponent<SpriteRenderer>();
+        sRend.sprite = newImage1;
+    }
+}
---
+++ COMP305Lab2/Assets/Scripts/Button3ClickHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button3ClickHandler : MonoBehaviour {
+
+    public GameObject gameObject3;
+
+    public Sprite newImage2;
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rbody = gameObject3.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        SpriteRenderer sRend = gameObject3.GetComponent<SpriteRenderer>();
+        sRend.sprite = newImage2;
+    }
+}
